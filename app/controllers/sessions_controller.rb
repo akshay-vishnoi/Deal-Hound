21de43895831b952216023fd505be850fe7d80b7
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-09-08 17:15:04 +0530

    db change, view format

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_name(params[:name])
-    if @user and @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       nxt_page = (@user.admin == 1) ? admins_url : users_url
       session[:user_id] = @user.id
       session[:admin] = @user.admin
