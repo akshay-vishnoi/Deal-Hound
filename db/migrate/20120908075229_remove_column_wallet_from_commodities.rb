fab6bdd8d1bd20b9700214519fb29a7819f57852
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-12-15 19:52:31 +0530

    removed unwanted migrations

--- db/migrate/20120908075229_remove_column_wallet_from_commodities.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnWalletFromCommodities < ActiveRecord::Migration
-  def up
-    remove_column :commodities, :wallet
-  end
-
-  def down
-    add_column :commodities, :wallet, :string
-  end
-end
