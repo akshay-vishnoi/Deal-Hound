5aa89b700cb205791886b44d7f8735b8f2b70836
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-12-15 18:59:30 +0530

    unicorn file added

---
+++ config/unicorn.rb
@@ -0,0 +1,35 @@
+app_path = "/var/www/apps/deal_hound/"
+
+user 'akshay'
+worker_processes 2
+
+working_directory "#{app_path}/current"
+
+listen "unix:#{app_path}/shared/tmp/sockets/unicorn.sock", :backlog => 64
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
+preload_app true
+
+timeout 30
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
