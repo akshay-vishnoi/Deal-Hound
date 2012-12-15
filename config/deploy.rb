250417ab0a9fc39e1670a568e0727af573414a7f
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-12-15 20:27:40 +0530

    minor changes regarding assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,6 @@ require 'bundler/capistrano'
 
 set :application, "deal_hound"
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :repository,  "git@github.com:akshay-vishnoi/Deal-Hound.git"
 set :branch, 'master'
 
@@ -20,21 +19,7 @@ set :deploy_via, :remote_cache
 set :keep_releases, 5
 server domain, :app, :web, :db, :primary => true
 
-
-# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-# role :app, "your app-server here"                          # This may be the same as your `Web` server
-# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-
-before 'deploy:restart', :copy_yml_files#, 'assets:precompile'
+before 'deploy:restart', :copy_yml_files, 'assets:precompile'
 after 'deploy:restart', 'unicorn:restart'
 
 task :copy_yml_files do
